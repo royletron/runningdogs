f785097c19b485b9aa81b8338d22478f54d9a342
Darren Royle <darren@easyreadsystem.com>
2016-04-18 14:04:56 +0100

    New assets

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum team {
+	home, away
+}
+
 public class PlayerController : MonoBehaviour {
 	public PlayerPosition position;
 	public float weight = 200.0f; //effects the players mass
@@ -9,14 +13,29 @@ public class PlayerController : MonoBehaviour {
 	public float dodge; 
 	public float stamina = 100.0f; // how far the player runs before slowing.
 	public float throwStrength;
+	public team side = team.home;
 	private Vector3 initialPos;
 	private Quaternion initialRotation;
+	public Sprite awaySprite;
+	public Sprite homeSprite;
 	// Use this for initialization
+	void Awake()
+	{
+		// load all frames in fruitsSprites array
+	}
 	void Start () {
 		PlayControl.players [PlayControl.counter] = this;
 		PlayControl.counter++;
 		initialPos = transform.position;
 		initialRotation = transform.rotation;
+	}
+
+	void OnValidate() {
+		if (this.side == team.home) {
+			this.GetComponent<SpriteRenderer> ().sprite = homeSprite;
+		} else {
+			this.GetComponent<SpriteRenderer> ().sprite = awaySprite;
+		}
 	}
 
 	public void Reset() {
